2ca4dcd24103b0f2851d62542296dc4853e2d47e
ofu997 <ofu997@gmail.com>
2020-04-30 21:55:36 -0700

    write resolvers

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -25,18 +25,39 @@ const typeDefs = gql`
   
   type Mission {
     name: String
-    missionPatch(size: PatchSize): String
-  }
+    missionPatch(mission: String, size: PatchSize): String
+  }  
   
   enum PatchSize {
     SMALL
     LARGE
   }  
+
   type Query {
-    launches: [Launch]!
+    launches( # replace the current launches query with this one.
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
     launch(id: ID!): Launch
     me: User
-  }  
+  }
+  
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection { # add this below the Query type as an additional type.
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
+  }
   type Mutation {
     bookTrips(launchIds: [ID]!): TripUpdateResponse!
     cancelTrip(launchId: ID!): TripUpdateResponse!
@@ -47,6 +68,7 @@ const typeDefs = gql`
     message: String
     launches: [Launch]
   }
+
 `;
 
 module.exports = typeDefs;
